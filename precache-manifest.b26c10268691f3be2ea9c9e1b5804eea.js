5adef4ce7edd67ca9efb4f577c3302cf58ad9543
juniajeff <juniajeff@gmail.com>
2024-10-16 22:54:31 +0900

    Updates

---
+++ precache-manifest.b26c10268691f3be2ea9c9e1b5804eea.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1678f43ac464bb1978f372b6fa972bc",
+    "url": "/portfolio_iuliia/index.html"
+  },
+  {
+    "revision": "752118f2855bcf902bf5",
+    "url": "/portfolio_iuliia/static/css/2.e2f2eb8e.chunk.css"
+  },
+  {
+    "revision": "7d63b7855f2977686557",
+    "url": "/portfolio_iuliia/static/css/main.8d1c7dce.chunk.css"
+  },
+  {
+    "revision": "752118f2855bcf902bf5",
+    "url": "/portfolio_iuliia/static/js/2.7f418a0d.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/portfolio_iuliia/static/js/2.7f418a0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fed8fe87f8f335ee77f6",
+    "url": "/portfolio_iuliia/static/js/3.3e100c92.chunk.js"
+  },
+  {
+    "revision": "7d63b7855f2977686557",
+    "url": "/portfolio_iuliia/static/js/main.138fcacb.chunk.js"
+  },
+  {
+    "revision": "42853b91221e9d33fc41",
+    "url": "/portfolio_iuliia/static/js/runtime-main.7d2c6079.js"
+  },
+  {
+    "revision": "56c8d80832e37783f12c05db7c8849e2",
+    "url": "/portfolio_iuliia/static/media/fa-brands-400.56c8d808.ttf"
+  },
+  {
+    "revision": "715d593456fa02fe72a008a72398f5be",
+    "url": "/portfolio_iuliia/static/media/fa-brands-400.715d5934.woff2"
+  },
+  {
+    "revision": "2d89b49ac28614e9ccd9c81308b5120c",
+    "url": "/portfolio_iuliia/static/media/fa-regular-400.2d89b49a.woff2"
+  },
+  {
+    "revision": "370dd5af19f8364907b6e2c41f45dbbf",
+    "url": "/portfolio_iuliia/static/media/fa-regular-400.370dd5af.ttf"
+  },
+  {
+    "revision": "237f4a0afbdb652fb2330ee7e1567dd3",
+    "url": "/portfolio_iuliia/static/media/fa-solid-900.237f4a0a.woff2"
+  },
+  {
+    "revision": "adec7d6f310bc577f05e8fe06a5daccf",
+    "url": "/portfolio_iuliia/static/media/fa-solid-900.adec7d6f.ttf"
+  },
+  {
+    "revision": "38fec6307db2547752379b51322ded10",
+    "url": "/portfolio_iuliia/static/media/fa-v4compatibility.38fec630.ttf"
+  },
+  {
+    "revision": "7c428138d15b872f193c40828f6fa0d3",
+    "url": "/portfolio_iuliia/static/media/fa-v4compatibility.7c428138.woff2"
+  }
+]);
